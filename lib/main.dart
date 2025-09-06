3de8db60b13fd83639a2d3c00c4d4b39f336c766
Klein Arnie Ortega <slozxklein@gmail.com>
2025-09-06 15:52:12 +0800

    Add floating action button to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Scaffold homepage = Scaffold(
-      appBar: AppBar(title: Text('Homepage'))
+      appBar: AppBar(title: Text('Homepage')),
+      floatingActionButton: FloatingActionButton(onPressed: (){},
+      child: const Icon(Icons.add)
+
+    ),
     );
-    
     return MaterialApp(
       home: homepage,
       theme: ThemeData(
